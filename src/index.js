3200fea175c72b996b958c2982840e9147546f92
Nick Kim <lignum.nlk@gmail.com>
2020-10-03 21:55:38 +0400

    refactor: increase readability

--- src/index.js
+++ src/index.js
@@ -1,40 +1,27 @@
 
 exports.min = function min (array) {
-  if (array) {
-  if (!array.length) {
-    return 0;
-  } else {
-    let result = array.sort( (a, b) => a - b );
+  if (array && array.length) {
+    const result = array.sort( (a, b) => a - b );
     return result[0];
+  } else {
+    return 0;
   }
-} else {
-  return 0;
-}
 }
 
 exports.max = function max (array) {
-  if (array) {
-  if (!array.length) {
-    return 0;
-  } else {
-  let result = array.sort( (a, b) => a - b );
+  if (array && array.length) {
+    const result = array.sort( (a, b) => a - b );
     return result[result.length - 1];
+  } else {
+    return 0;
   }
-} else {
-  return 0;
-}
 }
 
 exports.avg = function avg (array) {
-if (array) {
-  if (!array.length) {
-    return 0;
-  } else {
-    let result = array.reduce( (sum, currentItem) => sum + currentItem);
+  if (array && array.length) {
+    const result = array.reduce( (sum, currentItem) => sum + currentItem);
     return result / array.length;
+  } else {
+    return 0;
   }
-} else {
-  return 0;
 }
-}
-console.log('Pyosik');
